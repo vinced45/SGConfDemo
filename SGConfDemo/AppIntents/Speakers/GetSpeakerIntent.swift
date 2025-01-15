95bdb38185165fb72329646e9836e060ad3b2d78
Vince Davis <vince.davis@me.com>
2025-01-15 21:57:47 +0800

    Added missing compliance

--- SGConfDemo/AppIntents/Speakers/GetSpeakerIntent.swift
+++ SGConfDemo/AppIntents/Speakers/GetSpeakerIntent.swift
@@ -19,17 +19,13 @@ struct GetSpeakerIntent: AppIntent {
     @Parameter(title: "Speaker")
     var targetSpeaker: SpeakerEntity
 
-    func perform() async throws -> some IntentResult {
-        if !appState.path.isEmpty {
-            appState.path.removeLast()
-        }
-        
-        appState.path.append(targetSpeaker.id)
-        return .result()
+    func perform() async throws -> some IntentResult & ProvidesDialog & ShowsSnippetView & ReturnsValue<SpeakerEntity?> {
+        return .result(value: targetSpeaker,
+                       dialog: "\(targetSpeaker.name) is \(targetSpeaker.bio) ")
     }
     
     static var parameterSummary: any ParameterSummary {
-        Summary("Open \(\.$targetSpeaker)", table: "Select a speaker.")
+        Summary("Get \(\.$targetSpeaker)", table: "Select a speaker.")
     }
     
     @Dependency
